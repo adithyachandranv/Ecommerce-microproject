e5f9af1d7870a641f0d6fffadc5c815396d5fa09
adithyachandranv <adithyachandranv@gmail.com>
2023-12-08 18:07:47 +0530

    implemented auth

---
+++ file/resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    </style>
+</head>
+
+<body>
+    <div id="main">
+        <div id="main-container">
+            <div id="top-container">
+                <div id="nav">
+                    <ul>
+                        <li><a href="default.asp">Home</a></li>
+                        <li><a href="news.asp">News</a></li>
+                        <li><a href="contact.asp">Contact</a></li>
+                        <li><a href="about.asp">About</a></li>
+                </div>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
